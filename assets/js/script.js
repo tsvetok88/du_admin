e4c50120605bbd46cfb55d2e254a00960f59bd34
ihormihal <imykhalchenko@jayadigital.com>
2015-04-16 12:41:25 +0300

    reports

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,10 +23,67 @@ $(function(){
 	  "hideMethod": "fadeOut"
 	};
 
-	/* Documentation -> http://isocra.com/2008/02/table-drag-and-drop-jquery-plugin/ */
-	$('.dnd-table').tableDnD({
-		dragHandle: ".drag"
+	//Языковые настройки DataTables
+	var dataTablesLng = {
+		"emptyTable":     "Нет данных",
+		"info":           "Показано _START_ - _END_ из _TOTAL_",
+		"infoEmpty":      "Showing 0 to 0 of 0 entries",
+		"infoFiltered":   "(filtered from _MAX_ total entries)",
+		"infoPostFix":    "",
+		"thousands":      ",",
+		"lengthMenu":     "_MENU_",
+		"loadingRecords": "Загрузка...",
+		"processing":     "Обработка...",
+		"search":         "Поиск: ",
+		"zeroRecords":    "По данному запросу записей не найдено",
+		"paginate": {
+		  "first":      "Первая",
+		  "last":       "Последняя",
+		  "next":       "Следующая",
+		  "previous":   "Предыдущая"
+		},
+		"aria": {
+		  "sortAscending":  ": activate to sort column ascending",
+		  "sortDescending": ": activate to sort column descending"
+		}
+	};
+	//Набор и расположение элементов DataTables
+	var dataTablesDom = '<"table-top"f>t<"row"<"table-left col-md-6"p><"table-right col-md-6"l>><"clear">';
+
+	//Иницыализируем таблицу с данными, подгружаем JSON
+	var tableRates = $('#datatable-reports').DataTable({
+		ajax : $('#datatable-reports').data('json'),
+		columns : [
+			{	
+				data : "name",
+				class : "editable"
+			},
+			{
+				data : "visible",
+				class : "editable",
+				render : function (data, type, row) {
+					return data == 0 ? "Отобразить" : "Скрыть";
+				}
+			}
+		],
+		order : [ 1, "asc" ],
+		dom : dataTablesDom,
+		language : dataTablesLng,
+		//События при генерации рядка
+		fnCreatedRow: function( nRow, aData, iDataIndex ) {
+			$(nRow).addClass('roww');
+			console.log(nRow);
+			console.log(aData);
+			console.log(iDataIndex);
+		}
 	});
+
+	/* Documentation -> http://isocra.com/2008/02/table-drag-and-drop-jquery-plugin/ */
+	if($('.dnd-table').length > 0){
+		$('.dnd-table').tableDnD({
+			dragHandle: ".drag"
+		});
+	}
 
 	$('table td.clear').click(function(){
 		var row = $(this).parent('tr');
